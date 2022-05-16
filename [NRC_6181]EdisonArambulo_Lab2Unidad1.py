cf5088345c6ee81d5f5ce0ecbedabda2b6bd902f
Edisons Arambulo <eaar2300appol@gmail.com>
2022-05-15 19:38:58 -0500

    insertando_el_[NRC_6181]EdisonArambulo_Lab2Unidad1.py

---
+++ [NRC_6181]EdisonArambulo_Lab2Unidad1.py
@@ -0,0 +1,14 @@
+# ubicandonos desde  un angulo que no sea 90 grados
+def caratula():
+    #mensaje
+    print("UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE \n" +"viembenido, clacularas la hipotenusa de un triangulo rectangulo.\n autor: edison arambulo\n curso: POO, 2do 'a'")
+
+
+def hipotenusa(datOp, datAdya):
+    # calcular la hipotenusa con la formula de pitagoras
+    return((datOp**2 + datAdya**2)**1/2)
+
+caratula()
+catetOpDato = int(input("ingrese el cateto opuesto: "))
+catetAdDato = int(input("ingrese el cateteto adyasente: "))
+print("la hipotenusa del triangulo es: " + str(hipotenusa(catetOpDato, catetAdDato)))
